0bd77809f8250b31c332bd4e71d336a4b8c0b054
Adarsh Giri <adarsh2115ramesh@gmail.com>
2023-12-05 01:09:30 +0530

    Solved todo-list

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,60 @@
 */
 
 class Todo {
+  constructor()
+  {
+    this.arr = []
+  }
+  add(task)
+  {
+    
+    this.arr.push(task)
+  }
+  get(index)
+  {
+    if(this.arr[index])
+    {
+      return this.arr[index];
+    }
+    else
+    {
+      return null
+    }
+    
+  }
+  getAll()
+  {
+    return this.arr
+  }
 
+  remove(index)
+  {
+     this.arr.splice(index,1);
+  }
+   
+  update(index,updated_value)
+  {
+    if(this.arr[index])
+    {
+      this.arr[index] = updated_value;
+    }
+    
+  }
+  clear()
+  {
+    this.arr =[]
+  }
 }
 
+/**todoList = new Todo();
+
+todoList.add('Task 1');
+todoList.add('Task 2');
+todoList.add('Task 3');
+
+todoList.update(1, 'Updated Task 2');
+console.log(todoList.get(1))
+todoList.update(3, 'Invalid Task');
+console.log(todoList.getAll())**/
 module.exports = Todo;
+
