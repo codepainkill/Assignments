3c3838cfb820f438c0cb31e67650d661450c1f71
Adarsh Giri <adarsh2115ramesh@gmail.com>
2023-12-14 01:04:08 +0530

    last hard problem is remaining

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,18 +5,37 @@
  */
 
 function wait1(t) {
+    return new Promise(
+        function(resolve){
+            setTimeout(resolve,t*1000) 
+        }
+    )
 
 }
 
 function wait2(t) {
-
+    return new Promise(
+        function(resolve){
+            setTimeout(resolve,t*1000) 
+        }
+    )
 }
 
 function wait3(t) {
-
+    return new Promise(
+        function(resolve){
+            setTimeout(resolve,t*1000) 
+        }
+    )
 }
 
 function calculateTime(t1, t2, t3) {
+    const startTime = Date.now();
+      return Promise.all([wait1(t1),wait2(t2),wait3(t3)]).then(function time(){
+        const endTime = Date.now()
+        console.log(endTime-startTime)
+        return(endTime-startTime)
+      });
 
 }
 
