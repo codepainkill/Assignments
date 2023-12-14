966ff2b8da607b6c7450df3f526c5b48a5d2eba0
Adarsh Giri <adarsh2115ramesh@gmail.com>
2023-12-14 22:33:32 +0530

    Adding Some Code

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,35 +6,39 @@
  */
 
 function wait1(t) {
-    return new Promise(
-        function (resolve)
-        {
-            setTimeout(resolve,t*1000)
-        }
-    )
-
+    return new Promise(function (resolve) {
+        setTimeout(resolve, t);
+    });
 }
 
 function wait2(t) {
-    return new Promise(
-        function (resolve)
-        {
-            setTimeout(resolve,t*1000)
-        }
-    )
+    return new Promise(function (resolve) {
+        setTimeout(resolve, t);
+    });
 }
 
 function wait3(t) {
-    return new Promise(
-        function (resolve)
-        {
-            setTimeout(resolve,t*1000)
-        }
-    )
+    return new Promise(function (resolve) {
+        setTimeout(resolve, t);
+    });
 }
 
 function calculateTime(t1, t2, t3) {
+    const startTime = Date.now();
 
+    return wait1(t1*1000)
+        .then(() => wait2(t2*1000))
+        .then(() => wait3(t3*1000))
+        .then(() => {
+            const endTime = Date.now();
+            console.log("Time Difference: " + (endTime - startTime));
+            return endTime - startTime; // Return the time in milliseconds
+        });
 }
+
+// Example usage:
+calculateTime(1000, 2000, 3000).then((time) => {
+    console.log("Total Time: " + time + " ms");
+});
 
 module.exports = calculateTime;
